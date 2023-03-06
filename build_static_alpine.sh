d721d3f6e1a39dd6a71abf4598d586a13348aa40
txtsd <code@ihavea.quest>
2023-03-07 02:32:00 +0530

    fix(ci): Fix typo in generator spelling
    Signed-off-by: txtsd <code@ihavea.quest>

--- build_static_alpine.sh
+++ build_static_alpine.sh
@@ -2,5 +2,5 @@
 
 apk update
 apk add build-base cmake openssl openssl-dev openssl-libs-static linux-headers ninja-build
-cmake -B build -DCMAKE_BUILD_TYPE=RELEASE -DSTATIC_BINARY=true -G ninja .
+cmake -B build -DCMAKE_BUILD_TYPE=RELEASE -DSTATIC_BINARY=true -G Ninja .
 cmake --build build
