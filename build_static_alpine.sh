a1b78518287696a92819c86617f4ce40573faab0
txtsd <code@ihavea.quest>
2023-03-07 02:34:42 +0530

    fix(ci): Use alternate ninja package
    Signed-off-by: txtsd <code@ihavea.quest>

--- build_static_alpine.sh
+++ build_static_alpine.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
 apk update
-apk add build-base cmake openssl openssl-dev openssl-libs-static linux-headers ninja-build
+apk add build-base cmake openssl openssl-dev openssl-libs-static linux-headers ninja-is-really-ninja
 cmake -B build -DCMAKE_BUILD_TYPE=RELEASE -DSTATIC_BINARY=true -G Ninja .
 cmake --build build
