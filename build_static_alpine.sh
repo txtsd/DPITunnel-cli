a05a3626b5ce6ee0d703ff44671d0943b1b06dfe
txtsd <code@ihavea.quest>
2023-03-07 02:30:25 +0530

    chore(ci): Use ninja
    Signed-off-by: txtsd <code@ihavea.quest>

--- build_static_alpine.sh
+++ build_static_alpine.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
 apk update
-apk add build-base cmake openssl openssl-dev openssl-libs-static linux-headers
-cmake -B./build -DCMAKE_BUILD_TYPE=RELEASE -DSTATIC_BINARY=true .
-make -C ./build -j $(nproc)
+apk add build-base cmake openssl openssl-dev openssl-libs-static linux-headers ninja-build
+cmake -B build -DCMAKE_BUILD_TYPE=RELEASE -DSTATIC_BINARY=true -G ninja .
+cmake --build build
