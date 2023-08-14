2cbdefcc967f5cc3116d9b4d9bbc1680500fa692
txtsd <code@ihavea.quest>
2023-08-15 01:32:12 +0530

    fix(build): Install regular libnl3 too
    Signed-off-by: txtsd <code@ihavea.quest>

--- build_static_alpine.sh
+++ build_static_alpine.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
 apk update
-apk add build-base cmake openssl openssl-dev openssl-libs-static linux-headers ninja-is-really-ninja libnl3-dev
+apk add build-base cmake openssl openssl-dev openssl-libs-static linux-headers ninja-is-really-ninja linbl3 libnl3-dev
 cmake -B build -DCMAKE_BUILD_TYPE=RELEASE -DSTATIC_BINARY=true -G Ninja .
 cmake --build build
