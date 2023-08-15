b3d07e2e3b22efca3c9ad260d79055f44f628464
txtsd <code@ihavea.quest>
2023-08-15 13:07:39 +0530

    feat: Build, install, and use static libnl3
    Signed-off-by: txtsd <code@ihavea.quest>

--- build_static_alpine.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/ash
-
-apk update
-apk add build-base cmake openssl openssl-dev openssl-libs-static linux-headers ninja-is-really-ninja
-cmake -B build -DCMAKE_BUILD_TYPE=RELEASE -DSTATIC_BINARY=true -G Ninja .
-cmake --build build
