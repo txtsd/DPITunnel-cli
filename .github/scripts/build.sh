52ff52bbeaea3d77657f0c51b9f84187f7a3d383
txtsd <code@ihavea.quest>
2023-09-06 13:56:13 +0530

    fix(build): Run more commands with runuser
    Signed-off-by: txtsd <code@ihavea.quest>

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -18,10 +18,10 @@ echo "abuilder ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
 # Build static libnl3
 runuser -u abuilder -- /usr/bin/abuild-keygen -n -a -i -b 4096
-git clone https://gitlab.alpinelinux.org/alpine/aports.git --depth 1
+runuser -u abuilder -- git clone https://gitlab.alpinelinux.org/alpine/aports.git --depth 1
 cd aports/main/libnl3
 runuser -u abuilder -- /usr/bin/abuild -r
-apk add ~/packages/main/$(abuild -A)/libnl3*
+runuser -u abuilder -- apk add ~/packages/main/$(abuild -A)/libnl3*
 
 # Build static DPITunnel-cli
 cmake -B build -DCMAKE_BUILD_TYPE=RELEASE -DSTATIC_BINARY=true -G Ninja .
